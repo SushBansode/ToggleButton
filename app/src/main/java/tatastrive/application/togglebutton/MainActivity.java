fcb3bf3befb9aeb2f455d6f0c1cb6e6a9c8f3f0f
SushBansode <prajurm16@gmail.com>
2019-08-29 16:58:02 +0530

    Initial commit

---
+++ app/src/main/java/tatastrive/application/togglebutton/MainActivity.java
@@ -0,0 +1,28 @@
+package tatastrive.application.togglebutton;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+public class MainActivity extends AppCompatActivity {
+    ToggleButton toggleButton;
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        toggleButton=findViewById( R.id.toggleButton );
+        mediaPlayer=MediaPlayer.create( MainActivity.this,R.raw.maid );
+        toggleButton.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    mediaPlayer.start();
+                }else{
+                    mediaPlayer.pause();
+                }
+            }
+        } );
+    }
+}
